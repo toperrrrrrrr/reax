324bd464c2e186e8beb975926c9bf2a3fb8bffbf
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-07-22 00:04:07 +0800

    new beginnings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import NoPage from "./404";
+import Register from "./Register";
+import HorizontalScroll from "./HorizontalScroll";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/HorizontalScroll" element={<HorizontalScroll />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
