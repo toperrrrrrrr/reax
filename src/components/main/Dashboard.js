3243331b9cbe7dd0e703f0938c009136ab1cfb42
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-07-22 11:47:18 +0800

    SIDEBAR

--- src/components/main/Dashboard.js
+++ src/components/main/Dashboard.js
@@ -33,7 +33,7 @@ const Dashboard = ({ capturedtime }) => {
   return (
     <>
       <div class="wrapper ">
-        {/* <div class="left-side">
+        <div class="left-side">
           <svg
             viewBox="0 0 512 512"
             fill="currentColor"
@@ -61,7 +61,7 @@ const Dashboard = ({ capturedtime }) => {
             <path d="M255.2 468.6H63.8a21.3 21.3 0 01-21.3-21.2V64.6c0-11.7 9.6-21.2 21.3-21.2h191.4a21.2 21.2 0 100-42.5H63.8A63.9 63.9 0 000 64.6v382.8A63.9 63.9 0 0063.8 511H255a21.2 21.2 0 100-42.5z" />
             <path d="M505.7 240.9L376.4 113.3a21.3 21.3 0 10-29.9 30.3l92.4 91.1H191.4a21.2 21.2 0 100 42.6h247.5l-92.4 91.1a21.3 21.3 0 1029.9 30.3l129.3-127.6a21.3 21.3 0 000-30.2z" />{" "}
           </svg>
-        </div> */}
+        </div>
            <Sidebar />
 
         <div>
