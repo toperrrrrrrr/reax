65cb3797898da55fb2fcb39eb3e4739a191fc4cf
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-07-31 23:08:35 +0800

    Login page revamp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+import './util.css'
 
 import Login from "./components/logforms/Login";
 import Dashboard from "./components/main/Dashboard";
--- src/components/logforms/Login.js
+++ src/components/logforms/Login.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import Alerts from "../alerts/Alerts";
 import eyes from "../../img/eye.svg";
+import "../../fonts/material.css";
 
 const Login = () => {
   const [isLoggedIn, setLoggedIn] = useState(false);
@@ -63,103 +64,69 @@ const Login = () => {
   return (
     <>
       <div className="screen-bg">
-        <div class="login-box">
-       
-        <h2 className="center-text">Sign In</h2>
-          <form className="login-form">
-            <div class="user-box">
-              <input
-                type="text"
-                className="form-input"
-                required="" 
-                onChange={(e) => setUsername(e.target.value)}
-              ></input>
-              <label>Username</label>
-            </div>
-            <div class="user-box">
-               <input
-                type={isEye}
-                className="form-input"
-                onChange={(e) => setPassword(e.target.value)}
-              ></input>
-              <label>Password</label>
-            </div>
-            <a href="#">
-       
-              Submit
-            </a>
+        <div className="container-login100">
+          <div className="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+            <form className="login100-form">
+              <span className="login100-form-title p-b-49"> Login </span>
+              <div className="wrap-input100  m-b-23">
+                <span>Username</span>
+                <input
+                  className="input100"
+                  type="text"
+                  placeholder="Type your username"
+                  required=""
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <span className="focus-input100" data-symbol="&#xf206;"></span>
+              </div>
 
-            <div className="login-button-container">
-              <button type="button" className="form-btn" onClick={handleLogin}>
-                <div className="login-button">Login</div>
-              </button>
+              <div
+                className="wrap-input100"
+                data-validate="Password is required"
+              >
+                <span>Password</span>
+                <input
+                  className="input100"
+                  placeholder="Type your password"
+                  type={isEye}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <span className="focus-input100" data-symbol="&#xf190;"></span>
+              </div>
+
+              <div className="form-group d-md-flex m-t-30">
+                <div className="w-50">
+                  <input type="checkbox" name="remember" checked />
+                  <label
+                    className="checkbox-wrap checkbox-primary"
+                    for="remember"
+                  >
+                    Remember Me
+                  </label>
+                </div>
+                <div className="w-50 text-md-right">
+                  <a href="#">Forgot Password</a>
+                </div>
+              </div>
+              <div>
+                <div className="wrap-login100-form-btn" onClick={handleLogin}>
+                  <div className="login100-form-bgbtn"></div>
+                  <button className="login100-form-btn">Login</button>
+                </div>
+              </div>
+              <div className="text-center p-t-115">
+                <span className="txt1">Don’t have an account?</span>
+                <a className="txt2" onClick={register}>
+                  Sign Up
+                </a>
+              </div>
               <button type="button" className="eye-button" onClick={eye}>
                 <img src={eyes} className="eyes"></img>
               </button>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
 
-        <div class="limiter">
-		<div class="container-login100" style="background-color:darkslategrey;">
-			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-				<form class="login100-form validate-form">
-					<span class="login100-form-title p-b-49"> Login </span>
-
-					<div class="wrap-input100 validate-input m-b-23">
-						<span class="label-input100">Username</span>
-						<input class="input100" type="text" placeholder="Type your username" />
-						<span class="focus-input100" data-symbol="&#xf206;"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<span class="label-input100">Password</span>
-						<input class="input100" type="password" placeholder="Type your password" />
-						<span class="focus-input100" data-symbol="&#xf190;"></span>
-					</div>
-
-
-					<div class="form-group d-md-flex m-t-30" >
-						<div class="w-50">
-							<input type="checkbox" name="remember" checked />
-							<label class="checkbox-wrap checkbox-primary" for="remember">Remember Me</label>
-						</div>
-						<div class="w-50 text-md-right">
-							<a href="#">Forgot Password</a>
-						</div>
-					</div>
-					<div>
-						<div class="wrap-login100-form-btn">
-							<div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn">Login</button>
-						</div>
-					</div>
-					<div class="text-center p-t-115">
-						<span class="txt1">
-							Don’t have an account?
-						</span>
-
-						<a class="txt2" href="#">
-							Sign Up
-						</a>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-
-        {/* 
-
-
-            <div className="input-box input-container">
-              
-            </div>
-
-            <div className="form-btn" onClick={register}>
-              Register Here.
-    
-          </form>
-        </div> */}
         <Alerts
           isOpen={showPopup}
           title="Wrong Credentials"
