b4e9ee9aad4a1318fac5f24f3de1b3ab0f81e80b
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-08-04 22:11:40 +0800

    login layout updates

--- src/components/logforms/Login.js
+++ src/components/logforms/Login.js
@@ -64,17 +64,16 @@ const Login = () => {
   return (
     <>
       <div className="screen-bg">
-        <div className="container-login100">
-          <div className="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-            <form className="login100-form">
-              <span className="login100-form-title p-b-49"> Login </span>
-              <div className="wrap-input100  m-b-23">
+        <div className="container-login">
+          <form className="wrap-login">
+              <span className="signin-title"> Sign in </span>
+              <div className="wrap-input100">
                 <span>Username</span>
                 <input
                   className="input100"
                   type="text"
                   placeholder="Type your username"
-                  required=""
+                  required
                   onChange={(e) => setUsername(e.target.value)}
                 />
                 <span className="focus-input100" data-symbol="&#xf206;"></span>
@@ -89,6 +88,7 @@ const Login = () => {
                   className="input100"
                   placeholder="Type your password"
                   type={isEye}
+                  required
                   onChange={(e) => setPassword(e.target.value)}
                 />
                 <span className="focus-input100" data-symbol="&#xf190;"></span>
@@ -123,8 +123,8 @@ const Login = () => {
               <button type="button" className="eye-button" onClick={eye}>
                 <img src={eyes} className="eyes"></img>
               </button>
-            </form>
-          </div>
+     
+          </form>
         </div>
 
         <Alerts
