34f2ec37c1fd67f505db8c8ae8f59df5411d6a75
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-08-29 21:13:16 +0800

    need to work on the sessions

--- src/components/logforms/Login.js
+++ src/components/logforms/Login.js
@@ -6,7 +6,6 @@ import "../../fonts/material.css";
 import Axios from "axios";
 
 const Login = () => {
-  const [isLoggedIn, setLoggedIn] = useState(false);
   const [isUsername, setUsername] = useState("");
   const [isPassword, setPassword] = useState("");
   const [isPlaceholderPW, setPlaceholderPW] = useState("*******************");
@@ -14,8 +13,8 @@ const Login = () => {
   const [showPopup, setShowPopup] = useState(false);
   const [isResponse, setResponse] = useState([]);
   const navigate = useNavigate();
-  let username = "admin";
-  let password = "password";
+  const validUsername = "admin";
+  const validPassword = "password";
 
   const handleOpenPopup = () => {
     setShowPopup(true);
@@ -29,7 +28,7 @@ const Login = () => {
     handleClosePopup();
   };
 
-  const eye = () => {
+  const togglePasswordVisibility = () => {
     if (isEye === "password") {
       setEye("text");
       setPlaceholderPW("Type your password");
@@ -39,23 +38,17 @@ const Login = () => {
     }
   };
 
-  const register = () => {
+  const handleRegister = () => {
     navigate("/Register");
-    return <redirect to="/Register" />;
   };
 
   const handleLogin = () => {
-    if (username === isUsername && password === isPassword) {
-      setLoggedIn(true);
+    if (validUsername === isUsername && validPassword === isPassword) {
       navigate("/dashboard");
     } else {
       handleOpenPopup();
     }
   };
-
-  useEffect(() => {
-    fetchUsername();
-  }, []);
 
   const fetchUsername = async () => {
     try {
@@ -68,100 +61,100 @@ const Login = () => {
     }
   };
 
-  return (
-    <>
-      <div className="screen-bg">
-        <div className="container-login">
-          <div className="wrap-login">
-            <h1>
-              <div className="user-information">
-                <h2>User Information:</h2>
-                <div>
-                  {isResponse.map((val) => (
-                    <p key={val.iduser}>
-                      {val.iduser} {val.u_name}
-                    </p>
-                  ))}
-                </div>
-              </div>
-            </h1>
+  useEffect(() => {
+    fetchUsername();
+  }, []);
 
+  return (
+    <div className="screen-bg">
+      <div className="container-login">
+        <div className="wrap-login">
+          <h1 className="user-information">
+            <h2>User Information:</h2>
             <div>
-              <div className="wrap-login100-form-btn" onClick={fetchUsername}>
-                <div className="login100-form-bgbtn"></div>
-                <button className="login100-form-btn">fetchUsername</button>
-              </div>
+              {isResponse.map((val) => (
+                <p key={val.iduser}>
+                  {val.iduser} {val.u_name}
+                </p>
+              ))}
+            </div>
+          </h1>
+
+          <div>
+            <div className="wrap-login100-form-btn" onClick={fetchUsername}>
+              <div className="login100-form-bgbtn"></div>
+              <button className="login100-form-btn">Fetch Username</button>
+            </div>
+          </div>
+        </div>
+
+        <form className="wrap-login" onSubmit={handleLogin}>
+          <span className="signin-title"> Sign in </span>
+          <div className="wrap-input">
+            <span>Username</span>
+            <input
+              className="inputs"
+              type="text"
+              placeholder="Type your username"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <span className="loginicons" data-symbol="&#xf206;"></span>
+          </div>
+
+          <div className="wrap-input" data-validate="Password is required">
+            <span>Password</span>
+            <input
+              className="inputs"
+              type={isEye}
+              placeholder={isPlaceholderPW}
+              value={isPassword}
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <span
+              className="loginicons"
+              onClick={togglePasswordVisibility}
+              data-symbol="&#xf15c;"
+            ></span>
+          </div>
+
+          <div className="form-group d-md-flex m-t-30">
+            <div className="w-50">
+              <input type="checkbox" id="remember" className="cbox" />
+              <label className="cbxLabel" htmlFor="remember">
+                Remember Me
+              </label>
+            </div>
+            <div className="w-50 text-md-right">
+              <a href="#">Forgot Password</a>
+            </div>
+          </div>
+          <div>
+            <div className="wrap-login100-form-btn" onClick={handleLogin}>
+              <div className="login100-form-bgbtn"></div>
+              <button className="login100-form-btn">Login</button>
             </div>
           </div>
 
-          <form className="wrap-login" onSubmit={handleLogin}>
-            <span className="signin-title"> Sign in </span>
-            <div className="wrap-input">
-              <span>Username</span>
-              <input
-                className="inputs"
-                type="text"
-                placeholder="Type your username"
-                required
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <span className="loginicons" data-symbol="&#xf206;"></span>
-            </div>
-
-            <div className="wrap-input" data-validate="Password is required">
-              <span>Password</span>
-              <input
-                className="inputs"
-                type={isEye}
-                placeholder={isPlaceholderPW}
-                value={isPassword}
-                required
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <span
-                className="loginicons"
-                onClick={eye}
-                data-symbol="&#xf15c;"
-              ></span>
-            </div>
-
-            <div className="form-group d-md-flex m-t-30">
-              <div className="w-50">
-                <input type="checkbox" id="remember" className="cbox" />
-                <label className="cbxLabel" for="remember">
-                  Remember Me
-                </label>
-              </div>
-              <div className="w-50 text-md-right">
-                <a href="#">Forgot Password</a>
-              </div>
-            </div>
-            <div>
-              <div className="wrap-login100-form-btn" onClick={handleLogin}>
-                <div className="login100-form-bgbtn"></div>
-                <button className="login100-form-btn">Login</button>
-              </div>
-            </div>
-
-            <div className="text-center p-t-115">
-              <span className="txt1">Don’t have an account?</span>
-              <a className="txt2" onClick={register}>
-                Sign Up
-              </a>
-            </div>
-          </form>
-        </div>
-
-        <Alerts
-          isOpen={showPopup}
-          title="Wrong Credentials"
-          message="Please Double check"
-          onClose={handleClosePopup}
-          isHidden="true"
-          onConfirm={handleConfirmAction}
-        />
+          <div className="text-center p-t-115">
+            <span className="txt1">Don’t have an account?</span>
+            <a className="txt2" onClick={handleRegister}>
+              Sign Up
+            </a>
+          </div>
+        </form>
       </div>
-    </>
+
+      <Alerts
+        isOpen={showPopup}
+        title="Wrong Credentials"
+        message="Please Double check"
+        onClose={handleClosePopup}
+        isHidden="true"
+        onConfirm={handleConfirmAction}
+      />
+    </div>
   );
 };
 
